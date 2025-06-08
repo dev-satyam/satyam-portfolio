94793ebacb5a2c51d95386f2a05192fef2323167
dev-satyam <40088050+dev-satyam@users.noreply.github.com>
2025-06-08 17:22:54 +0530

    Create useThemeToggle.js

---
+++ src/hooks/useThemeToggle.js
@@ -0,0 +1,17 @@
+import { useMemo, useState } from "react";
+import ColorModeContext from "../theme/colorModeContext";
+import getTheme from "../theme/theme";
+
+export default function useThemeToggle() {
+  const [mode, setMode] = useState("light");
+  const colorMode = useMemo(
+    () => ({
+      toggleColorMode: () => {
+        setMode((prevMode) => (prevMode === "light" ? "dark" : "light"));
+      },
+    }),
+    []
+  );
+  const theme = useMemo(() => getTheme(mode), [mode]);
+  return { colorMode, theme };
+}
